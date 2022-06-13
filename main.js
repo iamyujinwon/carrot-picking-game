a7cb78e3510aac37927262be919e98f35675b3c7
Yujin Won <iamyujinwon@gmail.com>
2022-06-13 11:10:24 -0400

    set random location for carrots and bugs

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const item = document.querySelector('.item');
+const items = document.querySelectorAll('.item');
+const bugs = document.querySelectorAll('.bug');
 const counter = document.querySelector('.counter');
 const gameBtn = document.querySelector('.gameBtn');
 const timer = document.querySelector('.timer');
@@ -9,6 +10,8 @@ let isStart = false;
 let myinterval;
 let timeCounter = 10;
 
+const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
+
 gameBtn.addEventListener('click', () => {
   isStart = !isStart;
 
@@ -16,6 +19,18 @@ gameBtn.addEventListener('click', () => {
     gameBtn.innerHTML = `<i class="fa-solid fa-stop"></i>`;
     timer.innerHTML = `0:${timeCounter}`;
     myinterval = setInterval(setTimer, 1000);
+
+
+    for (i = 0; i < items.length; i++) {
+      items[i].style.left= getRandom(0, 1000)+'px'; 
+      items[i].style.top = getRandom(0, 800)+'px'; 
+    }
+
+    for (i = 0; i < bugs.length; i++) {
+      bugs[i].style.left= getRandom(0, 1000)+'px'; 
+      bugs[i].style.top = getRandom(0, 800)+'px'; 
+    }
+
   } else {
     gameBtn.style.visibility = 'hidden';
     modal.style.display = 'inline';
@@ -29,9 +44,11 @@ replayBtn.addEventListener('click', () => {
 
 
 
-item.addEventListener('click', () => {
-  setCounter();
-})
+for (i = 0; i < items.length; i++) {
+  items[i].addEventListener('click', () => {
+    setCounter();
+  })
+}
 
 function setCounter() {
   let carrotCounter = +counter.textContent;
